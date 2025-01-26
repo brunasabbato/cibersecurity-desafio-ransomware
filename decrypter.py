8b555bed9c26aa2e448a1b96324214f042f8dd9d
Bruna Sabbato <brunasabbato@gmail.com>
2025-01-26 00:28:46 -0300

    Entrega - Desafio Ransomware

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,22 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+# Abrir o arquivo criptografado
+file_name = "test_file.txt.thisisnotavirus"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
+# Chave descriptografia
+key = b"isnotavirustest"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+# Remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
+# Criar o arquivo descriptografado
+new_file = "test_file.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
